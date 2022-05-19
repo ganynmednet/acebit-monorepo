7a517606bfbf41792cfafd225f47dd3f307476d7
ganynmednet <Bsdjh381%>
2022-05-19 16:40:39 +0100

    presale deploy

--- packages/acebit-contracts/utils/constants.js
+++ packages/acebit-contracts/utils/constants.js
@@ -6,7 +6,7 @@ module.exports = Object(
             ACEBIT: "0xbEF42F43a5625fcfA822C7c8214565EC952cd682",
             aACEBIT: "0x0a2504aeAfAc0Ee1C5A84962cfd5395057A7005e",
             UserStorage: "0x31A378D024fe1A4f21205B2cA755F8F0a4DA9346",
-            Presale: "",
+            Presale: "0x2770a25250c747E30f7d7F4f6A921481814E4C14",
             DAO: "0xc8886947424eC1f0214aE9cF4A7e0BB296920A6b",
             DAI: "0x72727aA1Fa95660e7c67643eeF568891c3F936Fe"
         }
