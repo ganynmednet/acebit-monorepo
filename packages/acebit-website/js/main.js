781cc580d31b46fb742d2bfd37372df4bb0a82aa
ganynmednet <Bsdjh381%>
2022-06-10 14:51:00 +0100

    website => v1

---
+++ packages/acebit-website/js/main.js
@@ -0,0 +1,52 @@
+//Passes section start
+class cartAnimation {
+  constructor(cart,cursor){
+    this.cart = document.querySelector(cart)
+    this.cursor = document.querySelector(cursor)
+    this._render()
+  }
+  _render(){
+    this.cart.addEventListener('mousemove', e => {
+      this.cursor.style.cssText = `left: ${e.offsetX}px; top: ${e.offsetY}px; opacity: 1;`
+    })
+    this.cart.addEventListener('mouseleave', e => {
+      this.cursor.style.cssText = `left: ${e.offsetX}px; top: ${e.offsetY}px; opacity: 0;`
+    })
+  }
+}
+
+new cartAnimation('.heart-cart','.heart-cursor')
+new cartAnimation('.diamond-cart','.diamond-cursor')
+new cartAnimation('.club-cart','.club-cursor')
+new cartAnimation('.spade-cart','.spade-cursor')
+
+//Passes section end
+
+// Soon section start
+// const soonCard = document.querySelectorAll('.soon__card-inner')
+// const flipCart = () => {
+//   const cart = soonCard[Math.floor(Math.random() * soonCard.length)]
+//   setTimeout(() => {
+//     cart.classList.add('active')
+//     setTimeout(() => {
+//       cart.classList.remove('active')
+//       flipCart()
+//     }, 2000)
+//   }, 2000)
+// }
+// flipCart()
+// Soon section end
+
+
+
+// menu bg start
+let menu = document.getElementById("menu")
+addEventListener("scroll", () => {
+  const scrollWindow = window.pageYOffset
+  if(scrollWindow > 0) {
+    menu.classList.add("active-bg")
+  } else {
+    menu.classList.remove("active-bg")
+  }
+})
+// menu bg end
